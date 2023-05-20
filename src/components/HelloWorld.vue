3c2b8034782958a77e6e0222cb8a8f6af9fb60ac
antoinegilles <antoinegillesapb@hotmail.com>
2023-05-20 12:58:43 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center justify-center">
+      <v-col cols="7" class="tableau-resultats">
+        <h1>
+          {{ nomGrandPrix }} ({{ annee }})
+        </h1>
+        <v-table theme="dark" class="tableau-content">
+          <thead>
+            <tr>
+              <th class="text-left titre">Pilote</th>
+              <th class="text-left titre">Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in resultats" :key="index">
+              <td class="text-left">
+                {{ index + 1 }} - {{ item.Driver.givenName }}
+                {{ item.Driver.familyName }} ({{ item.Constructor.name }})
+              </td>
+              <td class="text-left">
+                {{ item.status == "Finished" ? item.Time?.time : item.status }}
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from "axios";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+export default {
+  name: "HelloWorld",
+
+  data() {
+    return {
+      circuit: [],
+      resultats: [],
+      infoGlobale: [],
+      nomGrandPrix: "",
+      annee:''
+    };
+  },
+  methods: {
+    getLastResult() {
+      var config = {
+        method: "get",
+        url: "http://ergast.com/api/f1/current/last/results.json",
+        headers: {},
+      };
+
+      axios(config)
+        .then((response) => {
+          console.log(response.data.MRData.RaceTable.Races[0].season);
+          console.log(response.data.MRData.RaceTable.Races[0]);
+          this.nomGrandPrix = response.data.MRData.RaceTable.Races[0].raceName;
+          this.resultats = response.data.MRData.RaceTable.Races[0].Results;
+          this.annee = response.data.MRData.RaceTable.Races[0].season
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getLastResult();
+  },
+};
+</script>
+<style >
+.tableau-resultats {
+  overflow: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.tableau-content{
+  background-color: #e10600;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.titre{
+  color: black !important;
+  font-weight: bold !important;
 }
 </style>
